c660e5018fafa5ebf0822dce7b95fcf741718647
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2022-10-28 19:38:01 +0200

    Add project files.

---
+++ JwtWebApiTut/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApiTut.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //[Authorize] ovde se ovo stavlja ako hocemo da celo ovaj controller zahteva autorizaciju
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast"), Authorize]//AllowAnonymous
+                                                         //ovo je kad ne moras da si registrovan da bi ovo koristio
+
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
